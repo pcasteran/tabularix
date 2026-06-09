3ce242090d7b2ad6ef58bed080a60292240554d6
Pascal Castéran <15686972+pcasteran@users.noreply.github.com>
2026-06-09 21:46:29 +0000

    up

--- python/tabularix/__init__.py
+++ python/tabularix/__init__.py
@@ -1,3 +1,3 @@
-from ._tabularix import load_workbook, Sheet, Workbook
+from ._tabularix import load_workbook, index_to_a1, a1_to_index, Sheet, Workbook
 
-__all__ = ["load_workbook", "Sheet", "Workbook"]
+__all__ = ["load_workbook", "index_to_a1", "a1_to_index", "Sheet", "Workbook"]
